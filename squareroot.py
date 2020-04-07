55eec1b33f30c93a9d0e44d11e4ba4a91db1a4e2
PhilipB777 <philipgbrady@gmail.com>
2020-04-07 19:25:33 +0100

    Square Root Approximation.

---
+++ squareroot.py
@@ -0,0 +1,28 @@
+# Philip Brady
+# This program takes a positive floating-point
+# number as input and outputs an approximation
+# of its square root.  The function called sqrt
+# does this.
+
+# Imported the math module.
+import math
+
+# Created a function that takes the value x.
+def sqrt(x):
+    # Used the sqrt function of the math module.
+    # Rounded answer to one decimal place.
+    answer = round(math.sqrt(x),1)
+    return answer
+
+# Created the variable number which would be
+# assigned the inputted floating point number.
+number = float(
+input("Please enter a positive number: "))
+
+# Called the sqrt function from the print function.
+print("The square root of {} is approx. {}."
+.format(number, sqrt(number)))
+
+# Reference:
+# “A Whirlwind Tour of Python by Jake VanderPlas (O’Reilly).
+# Copyright 2016 O’Reilly Media, Inc., 978-1-491-96465-1.”
