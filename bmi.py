519f93c20e28a7fc6caec4348199f3b499fe7b9c
PhilipB777 <philipgbrady@gmail.com>
2020-04-13 00:05:17 +0100

    BMI Calculator.

--- bmi.py
+++ bmi.py
@@ -1,9 +1,20 @@
-# This program takes height and weight from people
-# and calculates their BMI
+# Philip Brady
+# This program takes in the height and weight from people,
+# calculates their BMI and outputs the result.
 
-height = int(input("Enter your Height in cm: "))/100
+# The weight is taken in and converted to the int type.
 weight = int(input("Enter your Weight in kg: "))
+# The  height is taken in and converted to the int type
+# before the cm height is divided by 100 to convert it
+# to metres.
+height = int(input("Enter your Height in cm: "))/100
 
-bmi = round(weight / (height * height))
+# The weight is divided by height in metres squared.
+# It is rounded to two decimal places.
+bmi = round(weight / (height * height), 2)
 
-print("Your BMI is",bmi,)
+print("Your BMI is {}.".format(bmi))
+
+# Reference:
+# “A Whirlwind Tour of Python by Jake VanderPlas (O’Reilly).
+# Copyright 2016 O’Reilly Media, Inc., 978-1-491-96465-1.”
