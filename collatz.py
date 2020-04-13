5f9c261ef57ebb927685edf8c63d4dbce54ee29c
PhilipB777 <philipgbrady@gmail.com>
2020-04-13 22:53:00 +0100

    Outputs Successive Values

--- collatz.py
+++ collatz.py
@@ -1,13 +1,34 @@
 # Philip Brady
-# This is the Weekly Task 4
+# This is the Weekly Task 4.
+# The program takes in any positive integer
+# and outputs the successive values of the
+# following calculation:
+# At each step the next value is calculated
+# by taking the current value and if it is
+# even, divide it by two, but if it's odd,
+# multiply it by three and add one. The
+# program ends if the current value is one.
 
-result = input("Enter positive integer: ")
+# Take in the integer and assign it to result.
+result = input("Please enter a positive integer: ")
+# Convert to int type and assign it to number.
 number = int(result)
 
+# Initialise a while loop which continues until
+# the number is equal to 1.
 while number != 1:
-    if number % 2 == 0: 
-        number = number = number/2
+    # Check if number is even.
+    if number % 2 == 0:
+        # Calculation for even number.
+        number = number/2
     else:
+        # Calculation for odd number.
         number = (number*3) + 1
+    # Concatenate each value to the result string.
+    # This keeps the values on one line.
     result = result + " " + str(round(number))
-print(result)
+print("The successive values are {}.".format(result))
+
+# Reference:
+# “A Whirlwind Tour of Python by Jake VanderPlas (O’Reilly).
+# Copyright 2016 O’Reilly Media, Inc., 978-1-491-96465-1.”
